fba31519dbb2d85bcf39a669088cc60a3081e0a6
M. Mert Yildiran <me@mertyildiran.com>
2022-12-28 04:56:11 +0300

    :bug: Trim the environment variable values

--- src/types/global.ts
+++ src/types/global.ts
@@ -4,7 +4,7 @@ declare global {
   interface Window {
     __RUNTIME_CONFIG__: {
       NODE_ENV: string;
-      REACT_APP_DEFAULT_QUERY: string;
+      REACT_APP_DEFAULT_FILTER: string;
       REACT_APP_HUB_HOST: string;
       REACT_APP_HUB_PORT: string;
     };
