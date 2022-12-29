271017f1d48349b3d43896dce8354b9595288f0e
M. Mert Yildiran <me@mertyildiran.com>
2022-12-29 08:22:59 +0300

    :zap: Default Hub host to `window.location.hostname`

--- src/consts.tsx
+++ src/consts.tsx
@@ -1,5 +1,5 @@
-const HubHost = window.__RUNTIME_CONFIG__.REACT_APP_HUB_HOST ? window.__RUNTIME_CONFIG__.REACT_APP_HUB_HOST.trim() : "localhost" ;
-const HubPort = window.__RUNTIME_CONFIG__.REACT_APP_HUB_PORT ? window.__RUNTIME_CONFIG__.REACT_APP_HUB_PORT.trim() : "8898" ;
+const HubHost = window.__RUNTIME_CONFIG__.REACT_APP_HUB_HOST ? window.__RUNTIME_CONFIG__.REACT_APP_HUB_HOST.trim() : window.location.hostname;
+const HubPort = window.__RUNTIME_CONFIG__.REACT_APP_HUB_PORT ? window.__RUNTIME_CONFIG__.REACT_APP_HUB_PORT.trim() : "8898";
 const HubBaseUrl = `http://${HubHost}:${HubPort}`
 const HubWsUrl = `ws://${HubHost}:${HubPort}/ws`
 
